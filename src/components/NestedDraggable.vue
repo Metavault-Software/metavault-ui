cd4a46fc04c87c28f715b5313431df002bc2fc34
Derek Critchfield <decritc@gmail.com>
2023-07-22 10:02:13 -0500

    Adding task list

---
+++ src/components/NestedDraggable.vue
@@ -0,0 +1,50 @@
+<template>
+  <draggable
+    class="dragArea"
+    :list="tasks"
+    :group="{ name: 'g1' }"
+    :item-key="id"
+  >
+    <template #item="{element}">
+      <div class="px-5">
+        <div class="card list-group-item">
+          <div class="card-body">
+            <h5 class="card-title">
+              {{ element.name }}
+            </h5>
+            <p class="card-text">
+              {{ element.description }}
+            </p>
+            <a
+              href="#"
+              class="btn btn-primary"
+            >Go somewhere</a>
+          </div>
+        </div>
+        <NestedDraggable
+          v-if="element.children"
+          :tasks="element.children"
+        />
+      </div>
+    </template>
+  </draggable>
+</template>
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  name: "NestedDraggable",
+  components: {
+    draggable
+  },
+  props: {
+    tasks: {
+      required: true,
+      type: Array
+    }
+  }
+
+};
+</script>
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,75 +1,97 @@
 <template>
   <div class="home">
-    <img
-      alt="Vue logo"
-      src="../assets/logo.png"
-    >
-    <HelloWorld msg="Install Vue 3 with Bootstrap 5" />
-    <button
-      type="button"
-      class="btn btn-primary"
-    >
-      Primary
-    </button>
-    <button
-      type="button"
-      class="btn btn-secondary"
-    >
-      Secondary
-    </button>
-    <button
-      type="button"
-      class="btn btn-success"
-    >
-      Success
-    </button>
-    <button
-      type="button"
-      class="btn btn-danger"
-    >
-      Danger
-    </button>
-    <button
-      type="button"
-      class="btn btn-warning"
-    >
-      Warning
-    </button>
-    <button
-      type="button"
-      class="btn btn-info"
-    >
-      Info
-    </button>
-    <button
-      type="button"
-      class="btn btn-light"
-    >
-      Light
-    </button>
-    <button
-      type="button"
-      class="btn btn-dark"
-    >
-      Dark
-    </button>
+    <div class="row">
+      <div class="col-4">
+        <h3>Agents</h3>
+        <draggable
+          :list="taskSpecs"
+          class="dragArea list-group"
+          :item-key="id"
+          ghost-class="ghost"
+          :group="{ name: 'taskList', pull: 'clone', put: false }"
+        >
+          <template #item="{element}">
+            <div class="card list-group-item">
+              <div class="card-body">
+                <h5 class="card-title">
+                  {{ element.name }}
+                </h5>
+                <p class="card-text">
+                  {{ element.description }}
+                </p>
+              </div>
+            </div>
+          </template>
+        </draggable>
+      </div>
 
-    <button
-      type="button"
-      class="btn btn-link"
-    >
-      Link
-    </button>
+      <div class="col-8">
+        <h3>Current Process</h3>
+        <draggable
+          class="dragArea list-group"
+          :list="tasks"
+          group="taskList"
+          ghost-class="ghost"
+          item-key="id"
+        >
+          <template #item="{element}">
+            <div>
+              <div class="card list-group-item">
+                <div class="card-body">
+                  <h5 class="card-title">
+                    {{ element.name }}
+                  </h5>
+                  <p class="card-text">
+                    {{ element.description }}
+                  </p>
+                  <a
+                    href="#"
+                    class="btn btn-primary"
+                  >Go somewhere</a>
+                </div>
+              </div>
+              <NestedDraggable :tasks="element.children" />
+            </div>
+          </template>
+        </draggable>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
+ import draggable from 'vuedraggable';
+ import NestedDraggable from '@/components/NestedDraggable';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
+    draggable,
+    NestedDraggable
+  },
+  data() {
+    return { 
+      taskSpecs: [ 
+        {id: 1, name: "OpenAI Chat Task"}, 
+        { id: 2, name:"Google Translation Task"},
+        {id: 3, name: "WebSocket Server Task"}, 
+      ],
+      tasks: [
+        {id: 4, name: "OpenAI Chat Task", children: [{id: 6, name: "child", children: [{id: 7, name: "grandchild"}]}]}, 
+        {id: 5, name: "One Child Task", children: [{id: 6, name: "child"}]},
+        {id: 5, name: "No Child Task", children: []}
+      ]
+    }
 },
+methods: {
+  
+}
 };
 </script>
+
+<style>
+.card {
+  width: 360px;
+}
+
+</style>
